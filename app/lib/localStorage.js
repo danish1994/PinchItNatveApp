6f36c0afe0bd8b5865048ac5e605cf33cf7a70b1
Danish Kamal <danish8802204230@gmail.com>
2016-12-08 15:01:46 +0530

    React Updated

--- app/lib/localStorage.js
+++ app/lib/localStorage.js
@@ -1,17 +1,18 @@
 'use-strict'
 
 import { AsyncStorage } from 'react-native'
-// import Realm from 'realm';
+// import Realm from 'realm'
 
 export const loadState = () => {
   try {
-    AsyncStorage.getItem('state').then((value)=>{
-      console.log(value)
-      if (value === null){
-        return undefined
-      }
-      return JSON.parse(value)
+    const serializedState = AsyncStorage.getItem('state',()=>{
+      console.log('loaded')
     })
+    if (serializedState === null){
+        console.log('undefined')
+        return undefined
+    }
+    return JSON.parse(serializedState)
   }catch(err){
     console.log(err)
     return undefined
@@ -22,7 +23,7 @@ export const saveState = (state) => {
   try {
     const serializedState = JSON.stringify(state)
     AsyncStorage.setItem('state', serializedState).then(()=>{
-      console.log('saved')
+      console.log('State Saved')
     })
   }catch(err){
     console.log(err)
