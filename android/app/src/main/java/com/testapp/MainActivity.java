609b2db57946b07f255619a32ff9dddbc1eb46bf
Danish Kamal <danish8802204230@gmail.com>
2016-12-30 10:33:27 +0530

    react native updated

--- android/app/src/main/java/com/testapp/MainActivity.java
+++ android/app/src/main/java/com/testapp/MainActivity.java
@@ -1,8 +1,6 @@
 package com.testapp;
 
 import com.facebook.react.ReactActivity;
-import io.realm.react.RealmReactPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 
 public class MainActivity extends ReactActivity {
 
