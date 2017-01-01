5c236e2b371dbf8aa5d2b2b3467a69d076d5717e
Danish Kamal <danish8802204230@gmail.com>
2017-01-02 03:47:25 +0530

    New Screens Added

---
+++ app/screen/LoginScreen/index.js
@@ -0,0 +1,176 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Button,
+  TextInput,
+  Image
+} from 'react-native'
+
+import {connect} from 'react-redux'
+
+import ViewContainer from '../../containers/ViewContainer'
+
+const { width, height } = Dimensions.get('window')
+
+const windowWidth = Dimensions.get('window').width
+const windowHeight = Dimensions.get('window').height
+
+const background = require('./bg.png')
+const mark = require('./mark.png')
+const lockIcon = require('./lock.png')
+const personIcon = require('./person.png')
+
+class LoginScreen extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <ViewContainer>
+        <View style={styles.container}>
+          <Image source={background} style={styles.background} resizeMode='cover'>
+            <View style={styles.markWrap}>
+              <Image source={mark} style={styles.mark} resizeMode='contain' />
+            </View>
+            <View style={styles.wrapper}>
+              <View style={styles.inputWrap}>
+                <View style={styles.iconWrap}>
+                  <Image source={personIcon} style={styles.icon} resizeMode='contain' />
+                </View>
+                <TextInput
+                  placeholder='Username'
+                  placeholderTextColor='#FFF'
+                  style={styles.input}
+                />
+              </View>
+              <View style={styles.inputWrap}>
+                <View style={styles.iconWrap}>
+                  <Image source={lockIcon} style={styles.icon} resizeMode='contain' />
+                </View>
+                <TextInput
+                  placeholderTextColor='#FFF'
+                  placeholder='Password'
+                  style={styles.input}
+                  secureTextEntry
+                />
+              </View>
+              <TouchableOpacity activeOpacity={.5}>
+                <View>
+                  <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity activeOpacity={.5}>
+                <View style={styles.button}>
+                  <Text style={styles.buttonText}>Sign In</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.container}>
+              <View style={styles.signupWrap}>
+                <Text style={styles.accountText}>Dont have an account?</Text>
+                <TouchableOpacity activeOpacity={.5} onPress = {() => this._signUp()}>
+                  <View>
+                    <Text style={styles.signupLinkText}>Sign Up</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Image>
+        </View>
+      </ViewContainer>
+    )
+  }
+
+  _signUp(){
+    this.props.setActiveScreen('RegisterScreen')
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  markWrap: {
+    flex: 1,
+    paddingVertical: 30,
+  },
+  mark: {
+    width: null,
+    height: null,
+    flex: 1,
+  },
+  background: {
+    width,
+    height,
+  },
+  wrapper: {
+    paddingVertical: 30,
+  },
+  inputWrap: {
+    flexDirection: 'row',
+    marginVertical: 10,
+    height: 40,
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCC'
+  },
+  iconWrap: {
+    paddingHorizontal: 7,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    height: 20,
+    width: 20,
+  },
+  input: {
+    flex: 1,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: '#FF3366',
+    paddingVertical: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 30,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 18,
+  },
+  forgotPasswordText: {
+    color: '#D8D8D8',
+    backgroundColor: 'transparent',
+    textAlign: 'right',
+    paddingRight: 15,
+  },
+  signupWrap: {
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  accountText: {
+    color: '#D8D8D8'
+  },
+  signupLinkText: {
+    color: '#FFF',
+    marginLeft: 5,
+  }
+})
+
+
+function mapStateToProps(state){
+  return {
+    theme: state.theme.attributes
+  }
+}
+
+export default connect(mapStateToProps)(LoginScreen)
