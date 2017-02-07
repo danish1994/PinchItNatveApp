c005fc76b56c945845083255bc17d64b13ed0ec9
Danish Kamal <danish8802204230@gmail.com>
2017-02-07 23:54:23 +0530

    Push Notification Interface added

--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -13,32 +13,58 @@ import Api from '../lib/api'
 import BackgroundJob from "react-native-background-job"
 
 
-BackgroundJob.cancelAll();
-
-const backgroundJob = {
-  jobKey: 'loadPostBackground',
-  job: () => {
-    console.log('Loading Posts')
-    Api.get(`/post/`).then(resp => {
-      console.log(resp)
-    }).catch((err) => {
-      console.log(err)
-    })
-  }
-};
- 
-BackgroundJob.register(backgroundJob);
- 
-var backgroundSchedule = {
- jobKey: "loadPostBackground",
- timeout: 5000
-}
- 
-BackgroundJob.schedule(backgroundSchedule);
-
 class AppContainer extends Component {
     constructor(props) {
       super(props)
+
+      // Cancel All Background Tasks
+      BackgroundJob.cancelAll()
+
+      const backgroundJob = {
+        jobKey: 'loadPostBackground',
+        job: () => {
+          console.log('Loading Posts')
+          Api.get(`/post/`).then(resp => {
+            console.log(this.props.posts)
+            console.log(resp)
+
+            let newPosts = resp
+            let oldPosts = this.props.posts
+
+            if(oldPosts){
+              if(newPosts[newPosts.length-1].postid === oldPosts[oldPosts.length-1].postid){
+                console.log('no new post')
+                // this._pushNotification(newPosts[newPosts.length-1])
+              }else{
+                this._pushNotification(newPosts[newPosts.length-1])
+              }
+            }
+
+            
+          }).catch((err) => {
+            console.log(err)
+          })
+        }
+      }
+       
+      BackgroundJob.register(backgroundJob)
+       
+      var backgroundSchedule = {
+        jobKey: "loadPostBackground",
+        timeout: 5000,
+        period: 10000
+      }
+       
+      BackgroundJob.schedule(backgroundSchedule)
+
+    }
+
+    _pushNotification(post){
+      var PushNotification = require('react-native-push-notification')
+      PushNotification.localNotification({
+        title: post.title,
+        message: "New Posts Available",
+      })
     }
 
     render() {
@@ -51,7 +77,7 @@ class AppContainer extends Component {
 
 function mapStateToProps(state){
   return {
-
+    posts: state.posts,
   }
 }
 
--- index.android.js
+++ index.android.js
@@ -20,6 +20,28 @@ import reducer from './app/reducers'
 
 import AppContainer from './app/containers/AppContainer'
 
+// Register Push Notifications
+var PushNotification = require('react-native-push-notification');
+
+PushNotification.configure({
+  // (required) Called when a remote or local notification is opened or received
+  onNotification: function(notification) {
+      console.log( 'NOTIFICATION:', notification );
+  },
+
+  // Should the initial notification be popped automatically
+  // default: true
+  popInitialNotification: true,
+
+  /**
+    * (optional) default: true
+    * - Specified if permissions (ios) and token (android and ios) will requested or not,
+    * - if not, you must call PushNotificationsHandler.requestPermissions() later
+    */
+  requestPermissions: true,
+});
+
+
 const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__  })
 
 function configureStore(initialState) {
