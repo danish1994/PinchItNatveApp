764165d954fca8dc39c9e052ef6176ef8660b69b
Danish Kamal <danish8802204230@gmail.com>
2017-02-10 01:04:05 +0530

    Push Notification Updated

--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -13,77 +13,92 @@ import Api from '../lib/api'
 import BackgroundJob from "react-native-background-job"
 
 import {
-  ToastAndroid
+    ToastAndroid,
+    AsyncStorage
 } from 'react-native'
+
+// Cancel All Background Tasks
+BackgroundJob.cancelAll()
+
+const backgroundJob = {
+    jobKey: 'loadPostBackground',
+    job: () => {
+        AsyncStorage.getItem('state').then((state) => {
+            if (state) {
+                state = JSON.parse(state)
+                let currentPost = undefined
+                try {
+                    currentPost = state.posts[0]
+                } catch (err) {
+                    // console.log(err)
+                }
+                let url = `/post/`
+                if (currentPost) {
+                    url = `/post/?updatedAt=` + currentPost.updatedAt
+                }
+                Api.get(url).then(resp => {
+                    if (resp.length != 0) {
+                        for (let i = 0; i < resp.length; i++) {
+                            let post = resp[i]
+                            var PushNotification = require('react-native-push-notification')
+                            PushNotification.localNotification({
+                                title: "New Pinch",
+                                message: post.title,
+                                bigText: post.title
+                            })
+                        }
+                        resp.concat(state.posts)
+                        state.posts = resp
+                        AsyncStorage.setItem('state', JSON.stringify(state)).then(() => {
+                            console.log('State Saved')
+                        })
+                    }
+                }).catch((err) => {
+                    console.log(err)
+                })
+            }
+        }).catch((err) => {
+            console.log(err)
+        })
+
+    },
+    persist: false,
+    networkType: BackgroundJob.NETWORK_TYPE_UNMETERED
+}
+
+
+BackgroundJob.register(backgroundJob)
 
 
 class AppContainer extends Component {
     constructor(props) {
-      super(props)
+        super(props)
 
-      // Cancel All Background Tasks
-      BackgroundJob.cancelAll()
-
-      const backgroundJob = {
-        jobKey: 'loadPostBackground',
-        job: () => {
-          // ToastAndroid.show('Background Proccess Test.', ToastAndroid.SHORT)
-          let currentPost = this.props.posts[0]
-          let url = `/post/`
-          if(currentPost){
-            url = `/post/?updatedAt=` + currentPost.updatedAt 
-          }
-          Api.get(url).then(resp => {
-            // this.props.setPosts({ posts: resp }, true)
-            if(resp.length != 0){
-              this.props.setPosts({ posts: resp }, true)
-              for(let i=0; i<resp.length; i++){
-                this._pushNotification(resp[i])  
-              }
-            }
-          }).catch((err) => {
-            console.log(err)
-          })
+        var backgroundSchedule = {
+            jobKey: "loadPostBackground",
+            timeout: 5000,
+            period: 9000000
         }
-      }
-       
-      BackgroundJob.register(backgroundJob)
-       
-      var backgroundSchedule = {
-        jobKey: "loadPostBackground",
-        timeout: 5000,
-        period: 10000
-      }
-       
-      BackgroundJob.schedule(backgroundSchedule)
 
-    }
+        BackgroundJob.schedule(backgroundSchedule)
 
-    _pushNotification(post){
-      var PushNotification = require('react-native-push-notification')
-      PushNotification.localNotification({
-        title: "New Pinch",
-        message: post.title,
-        bigText: post.title
-      })
     }
 
     render() {
-      return(
-        <ApplicationTabs
-          { ...this.props } />
-      )
+        return ( < ApplicationTabs {...this.props }
+            />
+        )
     }
 }
 
-function mapStateToProps(state){
-  return {
-    posts: state.posts,
-  }
+function mapStateToProps(state) {
+    return {
+        posts: state.posts,
+    }
 }
 
-function mapDispatchToProps(dispatch){
-  return bindActionCreators(ActionCreators, dispatch)
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(ActionCreators, dispatch)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(AppContainer);
