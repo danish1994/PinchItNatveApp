a30c379f94ac5ce9cc959f56d121d80b3beab159
Danish Kamal <danish8802204230@gmail.com>
2017-02-14 01:41:49 +0530

    Background Task Removed

--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -17,55 +17,55 @@ import {
     AsyncStorage
 } from 'react-native'
 
-// Cancel All Background Tasks
-BackgroundJob.cancelAll()
+// // Cancel All Background Tasks
+// BackgroundJob.cancelAll()
 
-const backgroundJob = {
-    jobKey: 'loadPostBackground',
-    job: () => {
-        console.log('Background Task')
-        AsyncStorage.getItem('state').then((state) => {
-            if (state) {
-                state = JSON.parse(state)
-                let currentPost = undefined
-                try {
-                    currentPost = state.posts[0]
-                } catch (err) {
-                    // console.log(err)
-                }
-                let url = `/post/`
-                if (currentPost) {
-                    url = `/post/?updatedAt=` + currentPost.updatedAt
-                }
-                Api.get(url).then(resp => {
-                    if (resp.length != 0) {
-                        let counter = resp.length > 5 ? 5 : resp.length
-                        for (let i = 0; i < counter; i++) {
-                            let post = resp[i]
-                            var PushNotification = require('react-native-push-notification')
-                            PushNotification.localNotification({
-                                title: "New Pinch",
-                                message: post.title,
-                                bigText: post.title
-                            })
-                        }
-                        resp.concat(state.posts)
-                        state.posts = resp
-                        AsyncStorage.setItem('state', JSON.stringify(state)).then(() => {
-                            console.log('State Saved')
-                        })
-                    }
-                }).catch((err) => {
-                    console.log(err)
-                })
-            }
-        }).catch((err) => {
-            console.log(err)
-        })
+// const backgroundJob = {
+//     jobKey: 'loadPostBackground',
+//     job: () => {
+//         console.log('Background Task')
+//         AsyncStorage.getItem('state').then((state) => {
+//             if (state) {
+//                 state = JSON.parse(state)
+//                 let currentPost = undefined
+//                 try {
+//                     currentPost = state.posts[0]
+//                 } catch (err) {
+//                     // console.log(err)
+//                 }
+//                 let url = `/post/`
+//                 if (currentPost) {
+//                     url = `/post/?updatedAt=` + currentPost.updatedAt
+//                 }
+//                 Api.get(url).then(resp => {
+//                     if (resp.length != 0) {
+//                         let counter = resp.length > 5 ? 5 : resp.length
+//                         for (let i = 0; i < counter; i++) {
+//                             let post = resp[i]
+//                             var PushNotification = require('react-native-push-notification')
+//                             PushNotification.localNotification({
+//                                 title: "New Pinch",
+//                                 message: post.title,
+//                                 bigText: post.title
+//                             })
+//                         }
+//                         resp.concat(state.posts)
+//                         state.posts = resp
+//                         AsyncStorage.setItem('state', JSON.stringify(state)).then(() => {
+//                             console.log('State Saved')
+//                         })
+//                     }
+//                 }).catch((err) => {
+//                     console.log(err)
+//                 })
+//             }
+//         }).catch((err) => {
+//             console.log(err)
+//         })
 
-    },
-    persist: false
-}
+//     },
+//     persist: false
+// }
 
 
 BackgroundJob.register(backgroundJob)
@@ -75,13 +75,13 @@ class AppContainer extends Component {
     constructor(props) {
         super(props)
 
-        var backgroundSchedule = {
-            jobKey: "loadPostBackground",
-            timeout: 5000,
-            period: 9000000
-        }
+        // var backgroundSchedule = {
+        //     jobKey: "loadPostBackground",
+        //     timeout: 5000,
+        //     period: 9000000
+        // }
 
-        BackgroundJob.schedule(backgroundSchedule)
+        // BackgroundJob.schedule(backgroundSchedule)
 
     }
 
