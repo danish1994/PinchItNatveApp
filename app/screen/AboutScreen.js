bbeb22a7964d86fc416b2ccdacbc6c282b115ab5
Danish Kamal <danish8802204230@gmail.com>
2017-02-20 01:23:42 +0530

    Desihn Update

--- app/screen/AboutScreen.js
+++ app/screen/AboutScreen.js
@@ -30,7 +30,7 @@ class AboutScreen extends Component {
             About
           </Text>
           <Text style={[styles.text, this.props.theme]}>
-            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident.
+           Pinch is an app that searches, squeezes and then delivers you what matters you the most. We make you read worthy, get worthier and share the worthiest. Read in short or with elaboration {'\n'} - Choice is all yours.
           </Text>
         </View>
       </ViewContainer>
